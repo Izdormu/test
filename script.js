bb7be3c0b6121e2f1ba83eaeb10f870fe22b5009
Izdormu <ruheundstille@gmail.com>
2019-04-08 17:49:09 +0200

    Это наш первый коммит

---
+++ script.js
@@ -0,0 +1,24 @@
+'use strict'
+
+
+
+var number = 5;
+var string = "Hello";
+var sym= Symbol();
+var boolean = true;
+null;
+undefined;
+var obj = {};
+
+console.log (4/0);
+console.log ('string'*9);
+
+let something;
+console.log (something);
+
+let persone = {
+    name: "John",
+    age:25,
+    isMarried: false
+}
+console.log (persone.name);
